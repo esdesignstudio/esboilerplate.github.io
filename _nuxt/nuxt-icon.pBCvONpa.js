5a8a7bb19777241d0e785e946a0cec862455a758
Eason 🍎 <eason@e-s.tw>
2024-01-04 23:11:06 +0800

    fix LCscroll

---
+++ _nuxt/nuxt-icon.pBCvONpa.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./nuxt-icon.vue.pY7_m3x_.js";import"./entry.mgbIrPh7.js";import"./swiper-vue.gmmL4RLV.js";export{o as default};
---
+++ _nuxt/nuxt-icon.vue.pY7_m3x_.js
@@ -0,0 +1,7 @@
+import{s as f}from"./entry.mgbIrPh7.js";import{d as m,a as _,ac as p,y as d,N as h,O as x,a0 as y,u as r}from"./swiper-vue.gmmL4RLV.js";const I=["innerHTML"],v=m({__name:"nuxt-icon",props:{name:{},filled:{type:Boolean,default:!1}},async setup(l){let n,t;const o=l,a=_("");let c=!1;async function i(){try{const s=await Object.assign({"/assets/icons/logo.svg":()=>f(()=>import("./logo.4_Wdg4PW.js"),__vite__mapDeps([]),import.meta.url).then(u=>u.default)})[`/assets/icons/${o.name}.svg`]();s.includes("stroke")&&(c=!0),a.value=s}catch{console.error(`[nuxt-icons] Icon '${o.name}' doesn't exist in 'assets/icons'`)}}return[n,t]=p(()=>i()),await n,t(),d(i),(e,s)=>(h(),x("span",{class:y(["nuxt-icon",{"nuxt-icon--fill":!e.filled,"nuxt-icon--stroke":r(c)&&!e.filled}]),innerHTML:r(a)},null,10,I))}});export{v as _};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = []
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
